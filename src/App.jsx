80a83f63ae1a6b0b4c621c836ee2071f958f2c23
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2024-12-27 21:34:45 -0500

    added some comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // src/App.jsx
 
+// Importing the EventList component from the components folder
 import EventList from './components/EventList';
 
+// Main App component that serves as the root of our application
 const App = () => {
   return (
+    // className="App" helps identify this div as our app's container
     <div className="App">
+      {/* Rendering the EventList component */}
       <EventList />
     </div>
   );
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,7 +1,10 @@
 // src/components/EventList.jsx
 
+// This component will eventually display a list of events
 const EventList = () => {
     return (
+        // The id="event-list" is used by our tests to find this element
+        // Currently empty, but will later contain event items
         <div id="event-list"></div>
     );
 };
