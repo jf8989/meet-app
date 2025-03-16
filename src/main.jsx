ce5cd8262c9dc66a8764c621e4d6a61b6ebfea2e
Juan Francisco Marcenaro Arellano <juanfra2280@gmail.com>
2025-03-16 13:17:00 -0500

    Add Atatus for error tracking and update dependencies in package.json and package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 // src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as atatus from 'atatus-spa';
 import './index.css'
 import App from './App.jsx'
+
+atatus.config(import.meta.env.VITE_ATATUS_API_KEY).install();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
